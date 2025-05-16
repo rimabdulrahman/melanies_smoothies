1c11821b5621f5968a07b5bc616d3f6adbe60db9
rimabdulrahman <55586317+rimabdulrahman@users.noreply.github.com>
2025-05-16 15:41:53 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,8 @@ ingredients_list = st.multiselect(
 )
 
 if ingredients_list:
-      ingredients_string = ''
+    ingredients_string = ''
+
       for fruit_chosen in ingredients_list:
           ingredients_string += fruit_chosen + ' '
   
@@ -37,8 +38,8 @@ if ingredients_list:
           #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
   
           st.subheader(fruit_chosen + 'Nutririon Information')
-          fruityvice_response = requests.get("https://my.fruityvice.com/api/fruit/" + search_on)
-          fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width =True)
+          smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon" + search_on)
+          fv_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width =True)
 
 
       my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
